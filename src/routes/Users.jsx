1dfbf59242da9d4fac3ceb7d7663f28583f52222
Cristina Gallardo Garcia <cristig2@hotmail.es>
2023-08-16 19:24:12 +0100

    Browser Hook Improvements

--- src/routes/Users.jsx
+++ src/routes/Users.jsx
@@ -1,8 +1,10 @@
-import { NavLink, Outlet, useSearchParams } from "react-router-dom";
+import { NavLink, Outlet, useLocation, useSearchParams } from "react-router-dom";
 import { getAllUsers } from "../users"
 
 export function Users() {
     const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    
     const users = getAllUsers();
     const filter = searchParams.get("filter") ?? "";
 
@@ -12,8 +14,8 @@ export function Users() {
     return (
     <div>
         <h1>Users</h1>
-        <input value={filter} onChange={handleFilter} type="text" placeholder="filter"></input>
-        <div style={{display: "grid", gridTemplateColumns: "1fr 1fr", paddingTop: "10px"}}>
+        <input value={filter} onChange={handleFilter} type="text" placeholder="Write a name"></input>
+        <div style={{display: "grid", gridTemplateColumns: "1fr 1fr"}}>
         <ul>
             {users.filter(user => {
                 if (!filter) return true;
@@ -21,11 +23,11 @@ export function Users() {
                 const name = user.name.toLowerCase();
                 return name.includes(filter.toLowerCase());
             }).map(user => (
-            <li style={{listStyle: "None", paddingTop: "-10px"}}key={user.id}>
-                <h3>
+            <li style={{listStyle: "None"}}key={user.id}>
+                <h3 style={{ fontSize: "23px"}}>
                     <NavLink 
-                    style={{fontSize: "23px", marginTop: "0px"}} 
-                    to={user.id.toString()}>{user.name}
+                    style={({ isActive }) => (isActive ? {color: "black"} : {})} 
+                    to={user.id.toString() + location.search}>{user.name}
                     </NavLink>
                 </h3>
             </li>
